230d2f9ce876756f8c8a225fbd83423b156aaf8c
darrio-dk <darrio.dk@gmail.com>
2023-06-15 19:47:28 +0000

    Random package imported, hangman pictures plus name added

--- run.py
+++ run.py
@@ -1,3 +1,50 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import random
+
+HANGMAN_PICTURES = ['''
+    +---+
+         |
+         |
+         |
+        ===''', '''
+    +---+
+    O   |
+        |
+        |
+       ===''', '''
+    +---+
+    O   |
+    |   |
+        |
+       ===''', '''
+    +---+
+    O   |
+   /|   |
+        |
+       ===''', '''
+    +---+
+    O   |
+   /|\  |
+        |
+       ===''', '''
+    +---+
+    O   |
+   /|\  |
+   /    |
+       ===''', '''
+    +---+
+    O   |
+   /|\  |
+   / \  |
+       ===''']
+
+print(HANGMAN_PICTURES)
+
+
+# User enters their name to start the game
+name = input("Please enter your name: ")
+print("Hi",name,"" "lets begin the game")
+
